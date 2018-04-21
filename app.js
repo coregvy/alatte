4c308ad34561ae3c8b969d85f6922cbc3706cf29
yuskm <380pinkmoon053@gmail.com>
2018-04-21 18:51:18 +0900

    no message

--- app.js
+++ app.js
@@ -23,14 +23,18 @@ app.get('/', function(req, res){
 // tester
 app.get('/tester', function(req, res){
 //    res.sendfile('html/motiontest.html');
-    console.log("tester");
-    request.get('http://hackathon.support-cloud-projects.com/LaundromatWebApi/api/shopinfo?KNJPREFECTURES=東京都', {
+    var data = 'http://hackathon.support-cloud-projects.com/LaundromatWebApi/api/shopinfo?KNJPREFECTURES=' + encodeURIComponent("東京都");
+    console.log("tester" + data);
+    request.get(data, {
         headers: {
             Authorization:"Bearer vszudVcHhj8AQeH6qg3xXe-hUgEVu3uWq3QzTvNAEvSuON3haCjYpVbQX2sp0nDM_XbHPaCLTZxFYikxP1zYgcDQYo2xc4PTLdqFcRga9UfhWPNFMn561ZOEWHQfw-_0ZcAjSvnmg4S-QBTjl_Akcioi0fctND-nlUa2AEw2XH13hREtkRpzS2ss_4IxO1-tNXaJKO9P5s0VKKpALWP_j6CAnWsHUVVwpM7WNwBC5DxAr4aM9ljmyWVImptCx7MN2QuQYBQ5ZfRtMDd-nkp90r4Rg19lbe2WIcDSnQODReEvID459mfytYh94fwrXa6sWjKerd-Ao9M6_fVxAP5Hr52FCjuYX57j0-JM1wlZ7U6UmBLGtxMoxduAw9HKPeS1IaK7hTax4oTuGlnY8wr0oLl_2A8sc8Gu3zyV-QcyIcTdPQNwT0ogacjVW-Vw0hYym2cIgVo0Mh5nXbvFetBUwBNon2KSDOWnbUbKVDu5L98",
             Accept:"application/json"
         }
     }, (error, response, body) => {
-        console.log("err" + error);
+        console.log("err", error);
         console.log("res", response);
+
+        res.header('Content-Type', 'application/json; charset=utf-8')
+        res.send(response);
     });
 });
